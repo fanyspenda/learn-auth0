03f3f9789b209d41e28515b2aa300d73f319ec75
fanyspenda <fanyervansyah.9c@gmail.com>
2020-09-01 23:04:44 +0700

    feat(login): useAuth0 for Login Button

--- src/pages/loggedOut.tsx
+++ src/pages/loggedOut.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LoggedOut: React.FunctionComponent = () => {
+	const { loginWithRedirect } = useAuth0();
 	return (
 		<>
 			<h1>You are not Logged In if You See This</h1>
-			<a href="#">Log In</a>
+			<button onClick={() => loginWithRedirect()}>Log In</button>
 		</>
 	);
 };
